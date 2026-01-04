1e07d91a96eb359facdae451f88af5f9a8f4156b
Colin Melville <colintmelville@gmail.com>
2026-01-04 20:20:41 +0800

    chore: add debug-config route

--- jwa-demo/routes/web.php
+++ jwa-demo/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::get('debug-config', function () {
+    return [
+        'api_enabled' => config('statamic.api.enabled'),
+        'api_route' => config('statamic.api.route'),
+        'api_middleware' => config('statamic.api.middleware'),
+        'collections' => \Statamic\Facades\Collection::all()->map->handle()->values(),
+        'env_api_enabled' => env('STATAMIC_API_ENABLED'),
+    ];
+});
