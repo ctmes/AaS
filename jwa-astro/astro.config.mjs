3f20a2423501c146796b8b036f5b45624a65391b
Colin Melville <colintmelville@gmail.com>
2026-01-14 13:45:28 +0800

    fix formatting and update links/redirects

--- jwa-astro/astro.config.mjs
+++ jwa-astro/astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  redirects: {
+    '/bog-mats/dura-base-matting': '/products/dura-base'
+  }
 });
